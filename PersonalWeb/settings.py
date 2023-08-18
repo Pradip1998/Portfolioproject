0949391f58b18ef60aa285f489667daf37a5e0c2
Pradip1998 <pradipchapagain981@gmail.com>
2023-08-18 23:03:04 +1000

    Index is completed

--- PersonalWeb/settings.py
+++ PersonalWeb/settings.py
@@ -118,3 +118,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS= [
+    BASE_DIR/'static'
+
+]
+STATIC_ROOT=BASE_DIR/'assets'
+
+MEDIA_URL = '/images/'
+STATICFILES_DIRS=[BASE_DIR/'static']
+MEDIA_ROOT=BASE_DIR/'images'
