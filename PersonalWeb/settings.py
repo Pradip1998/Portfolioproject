a1353eea4f733711c7dfc7ec4de4ab131aace263
Pradip1998 <pradipchapagain981@gmail.com>
2023-09-12 11:22:33 +1000

    Modified

--- PersonalWeb/settings.py
+++ PersonalWeb/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'rolhyz3o=7z0bv4qw4xmx%5ygy(an(phs$7p853q5u5+*3#b)+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
